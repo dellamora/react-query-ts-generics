b786f67e3e8ffd31f8ac2e858c8769ee9c0ebb25
Dellamora <francielle@dellamora.dev>
2023-01-09 15:13:52 -0300

    added next link and changed the logo size

--- src/modules/nav/index.tsx
+++ src/modules/nav/index.tsx
@@ -9,28 +9,34 @@ const Nav: React.FC = (): JSX.Element => {
   return (
     <div className="relative flex justify-between h-50 items-center  py-2 px-8 lg:px-16 ">
       <a href="">
-       <Image alt="rick and morty log" src="/logoRAM.png" width={180} height={55} />
+       <Image alt="rick and morty log" src="/logoRAM.png" width={150} height={55} />
       </a>
       <button className="block lg:hidden" onClick={() => setIsOpen(current => !current)}>
         X
       </button>
       {isOpen && 
-        <div className="block lg:hidden absolute top-10 left-0 right-0 bg-white h-[90vh] font-semibold text-2xl flex-col gap-4 items-center ">
+        <div className=" lg:hidden absolute top-20 left-0 right-0 h-screen w-screen font-semibold text-2xl flex flex-col gap-4 py-4 items-center bg-primary">
           <Link href="/characteres">
-           Characteres
+           <h1>Characteres </h1>
           </Link>
-          <a href="/locations">
+          <Link href="/locations">
            <h1>Locations</h1>
-          </a>
-          <a href="/epsiodes">
+          </Link>
+          <Link href="/epsiodes">
            <h1>Epsiodes</h1>
-          </a>
+          </Link>
         </div>
       }
       <div className="hidden lg:flex justify-between gap-4 text-lg font-semibold">
-      <h1>Characteres</h1>
-      <h1>Locations</h1>
-      <h1>Epsiodes</h1>
+          <Link href="/characteres">
+           Characteres
+          </Link>
+          <Link href="/locations">
+           Locations
+          </Link>
+          <Link href="/epsiodes">
+           Epsiodes
+          </Link>
       </div>
     </div>
   );
