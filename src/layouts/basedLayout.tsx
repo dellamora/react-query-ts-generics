6b9986719ef4c733f508ec10f5cc4672f10749e3
Dellamora <francielle@dellamora.dev>
2023-01-09 14:59:36 -0300

    added layout component

---
+++ src/layouts/basedLayout.tsx
@@ -0,0 +1,29 @@
+// import { useScroll } from "framer-motion";
+import React, { useRef } from "react";
+import Nav from "../modules/nav";
+import Footer from "../modules/footer";
+const BaseLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
+  const containerRef = useRef(null);
+//   const { scrollYProgress } = useScroll({
+//     container: containerRef,
+//   });
+  return (
+    <div className="h-screen w-screen overflow-y-hidden">
+      <div id="modal-root" />
+      <div
+        className="h-screen flex flex-col bg-primary overflow-auto "
+        ref={containerRef}
+      >
+        <Nav/>
+        <div className="mt-[64px]">{children}</div>
+        <Footer/>
+      </div>
+    </div>
+  );
+};
+
+export default BaseLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../common/styles/globals.css";
+import BaseLayout from "../layouts/basedLayout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <BaseLayout>
+     <Component {...pageProps} />;
+    </BaseLayout>
+  )
 };
 
 export default MyApp;
