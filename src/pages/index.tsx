e7eca1d525e144f8395235a6c55195b9278ed231
Dellamora <francielle@dellamora.dev>
2023-01-09 15:01:16 -0300

    removed nav and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import { type NextPage } from "next";
 import Footer from "../modules/footer";
-import Nav from "../modules/nav";
 import Hero from "../modules/hero";
 
 const Home: NextPage = () => {
   return (
 <div>
-  <Nav/>
   <Hero/>
-  <Footer/>
 </div>
   );
 };
