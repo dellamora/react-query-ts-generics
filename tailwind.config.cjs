fa57d98e0e3f7e24623fa7036030a4abdb535507
Dellamora <francielle@dellamora.dev>
2023-01-09 14:59:23 -0300

    added global theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#181818",
+      primaryRed: "#C20000",
+      TextWhite: "#F1F1F1",
+    },
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
