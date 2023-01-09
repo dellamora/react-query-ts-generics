95a5b89759f87dc710ed92b54817ebad4c7ea592
Dellamora <francielle@dellamora.dev>
2023-01-09 16:41:28 -0300

    added search component at hero section

--- src/modules/hero/index.tsx
+++ src/modules/hero/index.tsx
@@ -2,15 +2,17 @@
 
 import React from "react";
 import Image from "next/image";
+import SearchComponent from "../../common/components/search";
 
 /* import { Container } from "./styles"; */
 
 const hero: React.FC = (): JSX.Element => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center items-center">
       <div className="relative w-72 h-56">
       <Image src="/portalRAM.png" fill alt="rick and morty title"/>
       </div>
+      <SearchComponent/>
     </div>
   );
 };
