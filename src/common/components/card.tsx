31ac3e63268f925f1b46f500c3674743d6be03e9
Dellamora <francielle@dellamora.dev>
2023-01-09 16:41:05 -0300

    add card component

---
+++ src/common/components/card.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const components: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, components!</h1>
+    </div>
+  );
+};
+
+export default components;
