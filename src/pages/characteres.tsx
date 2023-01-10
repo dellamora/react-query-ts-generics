e83c480bdff9b21fa59bb7a74606bfcd02d55839
Dellamora <francielle@dellamora.dev>
2023-01-10 09:47:55 -0300

    added new pages

---
+++ src/pages/characteres.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+import Image from "next/image";
+import SearchComponent from "../common/components/search";
+
+/* import { Container } from "./styles"; */
+
+const Characteres: React.FC = (): JSX.Element => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="relative w-72 h-56">
+      <Image src="/portalRAM.png" fill alt="rick and morty title"/>
+      </div>
+      <SearchComponent/>
+    </div>
+  );
+};
+
+export default Characteres;
---
+++ src/pages/episodes.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const pages: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, pages!</h1>
+    </div>
+  );
+};
+
+export default pages;
