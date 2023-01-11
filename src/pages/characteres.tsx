81211b8f05d2ffbc6338fa7f1e4618e1858ea656
Dellamora <francielle@dellamora.dev>
2023-01-11 09:56:55 -0300

    added space at each component at chacactere page

--- src/pages/characteres.tsx
+++ src/pages/characteres.tsx
@@ -9,9 +9,9 @@ import CardCharactere from "../common/components/cardCharactere";
 
 const Characteres: React.FC = (): JSX.Element => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="relative w-72 h-56">
-      <Image src="/portalRAM.png" fill alt="rick and morty title"/>
+    <div className="flex flex-col justify-center items-center space-y-6">
+      <div className="relative w-72 h-60">
+      <Image src="/portalRAM.png" fill alt="rick and morty title" className="aspect-square"/>
       </div>
       <SearchComponent/>
       <CardCharactere/>
