a6f74b9c870650a3aac3e5dfb7286d9ecc8ad4e0
Dellamora <francielle@dellamora.dev>
2023-01-11 09:56:27 -0300

    finish layout details

--- src/common/components/cardCharactere.tsx
+++ src/common/components/cardCharactere.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const CardCharactere: React.FC = (): JSX.Element => {
   return (
-    <div className="h-72 w-80 rounded overflow-hidden border-2 border-primary bg-secondary">
+    <div className="h-72 w-80 rounded-md overflow-hidden border-2 border-primary bg-secondary">
       <div className="aspect-video bg-primaryRed"> imagem aqui</div>
       <div className=" w-full h-full p-4">
         <h1 className="font-bold">nome do pp</h1>
