58032ee14f78f6f1cc6679d1a1f8a8e10a5699f2
Dellamora <francielle@dellamora.dev>
2023-01-12 19:44:56 -0300

    [WIP] comecei a consumir  a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import { type NextPage } from "next";
-import Footer from "../modules/footer";
+import { useState, useEffect } from "react";
 import Grid from "../modules/hero/grid";
 
+const APILInk = "https://rickandmortyapi.com/api/character/?page=1";
+
+
 const Home: NextPage = () => {
+  const [infoData, setInfoData] = useState([]);
+
+  useEffect(() => {
+    fetch(APILInk)
+      .then((response) => response.json())
+      .then((response) => {
+        setInfoData(response.results);
+        console.info(response.results);
+      });
+  }, []);
+  
   return (
-<div>
+ <div>
   <Grid/>
-</div>
+ </div>
   );
 };
 
