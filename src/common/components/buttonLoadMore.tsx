4d790271bfc4572db33b9f28902e9a79a0e1d0b4
Dellamora <francielle@dellamora.dev>
2023-01-12 19:59:05 -0300

    added buton load more component

---
+++ src/common/components/buttonLoadMore.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const ButtonLoadMore: React.FC = (): JSX.Element => {
+  return (
+    <button>
+        load more
+    </button>
+  );
+};
+
+export default ButtonLoadMore;
