8e99e5f31cae5647b41f578cc32ff8af0aeae86b
Dellamora <francielle@dellamora.dev>
2023-01-12 20:04:19 -0300

    added button at grid component

--- src/modules/hero/grid.tsx
+++ src/modules/hero/grid.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import SearchComponent from "../../common/components/search";
 import { motion } from "framer-motion";
+import ButtonLoadMore from "../../common/components/buttonLoadMore";
 
 const cards = [
   {
@@ -65,24 +66,27 @@ const cards = [
 
 const Grid: React.FC = (): JSX.Element => {
   return (
-   <motion.div 
-    id="grid"
-    className="p-10 h-fit"
-   >
+   <>
+   <motion.div
+      id="grid"
+      className="p-10 h-fit"
+    >
       <div className="grid grid-cols-cards gap-10">
         {cards.map((card, i) => {
           return (
-              <div
-                className="rounded-lg overflow-hidden h-20 w-20 bg-TextWhite"
-                key={`card-${i}`}
-              >
+            <div
+              className="rounded-lg overflow-hidden h-20 w-20 bg-TextWhite"
+              key={`card-${i}`}
+            >
               oi
-              </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
+      </div>
 
-   </motion.div>
+    </motion.div>
+    <ButtonLoadMore />
+    </>
  
   );
 };
