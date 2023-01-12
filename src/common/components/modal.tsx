9c30b89830c4aceffe98ae76e8136bbaa5732a1f
Dellamora <francielle@dellamora.dev>
2023-01-12 19:57:43 -0300

    [WIP] charactere modal info

--- src/common/components/modal.tsx
+++ src/common/components/modal.tsx
@@ -6,8 +6,24 @@ import React from "react";
 
 const Modal: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1> Hello, Modal!</h1>
+    <div className="w-full h-screen p-10">
+        <div>go back</div>
+        <div className="flex flex-col items-center">
+            <div className="rounded-full bg-primaryRed h-32 w-32"></div>
+            <h1 className="text-TextWhite text-2xl">Nome do personagem</h1>
+        </div>
+        <div>
+            <h1>Informations</h1>
+            <div>
+
+            </div>
+        </div>
+        <div>
+            <h1>Episodes</h1>
+            <div>
+                
+            </div>
+        </div>
     </div>
   );
 };
--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
+import Modal from "../common/components/modal";
 
 /* import { Container } from "./styles"; */
 
 const pages: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1> Hello, pages!</h1>
-    </div>
+  <Modal/>
   );
 };
 
