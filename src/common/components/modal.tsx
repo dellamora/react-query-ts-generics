7abb8608cd5a3dd4f614256830edf14b0e33fc39
Dellamora <francielle@dellamora.dev>
2023-01-12 19:51:53 -0300

    added component modal

---
+++ src/common/components/modal.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const Modal: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, Modal!</h1>
+    </div>
+  );
+};
+
+export default Modal;
