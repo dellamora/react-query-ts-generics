f9fa633cf684bcdcb2abfb19a0dad2c5553d2f31
Dellamora <francielle@dellamora.dev>
2023-01-13 22:53:32 -0300

    create card location component

---
+++ src/common/components/cardLocation.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const CardLocation: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, CardLocation!</h1>
+    </div>
+  );
+};
+
+export default CardLocation;
