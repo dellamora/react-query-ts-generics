5de7cdc88b3d9d5267905c2d4a127a89a21214fc
Dellamora <francielle@dellamora.dev>
2023-01-17 11:14:47 -0300

    added rick and morty API

---
+++ src/domain/interfaces.tsx
@@ -0,0 +1,36 @@
+export interface RickAndMorty {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   Gender;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  string;
+  }
+  
+  export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Unknown = "unknown",
+  }
+  
+  export interface Location {
+    name: string;
+    url:  string;
+  }
+  
+  export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+  }
+  
+  export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+  }
