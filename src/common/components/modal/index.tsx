223f0e374bdc51cff4083de0e6496969ca1dd5a9
Dellamora <francielle@dellamora.dev>
2023-02-01 22:25:31 -0300

    created a reactportal for modal

--- src/common/components/modal/index.tsx
+++ src/common/components/modal/index.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import ArrowBack from "../../svgs/arrow-back";
 /* import { Container } from "./styles"; */
 
 const Modal = (): JSX.Element => {
-
-  return (
-    <div className="absolute isent-0 z-50 bg-primary w-full h-screen p-10">
-        <div>go back</div>
+    const [mount, setMount] = useState(false);
+    useEffect(() => {
+      setMount(true);
+    }, []);
+    if (!mount) {
+      return <></>;
+    }
+  
+    return createPortal(
+        <>
+       <div className="absolute isent-0 z-50 bg-primary w-full h-screen p-4">
+        <ArrowBack/>
         <div className="flex flex-col items-center">
-            <div className="rounded-full bg-primaryRed h-32 w-32"></div>
+            <div className="rounded-full bg-primaryRed  h-28 w-28"></div>
             <h1 className="text-TextWhite text-2xl">Nome do personagem</h1>
         </div>
         <div>
@@ -27,6 +36,8 @@ const Modal = (): JSX.Element => {
             </div>
         </div>
     </div>
+    </>,
+     document.getElementById("modal-root"),
   );
 };
 
