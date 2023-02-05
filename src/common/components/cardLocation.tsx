1a9c49fd59834cd01c228f7fccd1c3eac1243f8f
Dellamora <francielle@dellamora.dev>
2023-02-04 21:34:01 -0300

    [WIP] location's card integration

--- src/common/components/cardLocation.tsx
+++ src/common/components/cardLocation.tsx
@@ -5,10 +5,21 @@ import React from "react";
 /* import { Container } from "./styles"; */
 
 const CardLocation: React.FC = (): JSX.Element => {
+  const locations = [
+    {location: "one", ep: "232"},
+    {location: "two", ep: "223"},
+  ]
   return (
     <div className="h-28 w-80 p-4 flex flex-col justify-center items-center rounded-md border-2 border-primary bg-secondary">
-        <h1 className="font-bold">Location: example</h1>
-        <h1 className="text-textGray">EP: 12S3</h1>
+       {locations.map(location => {
+        return (
+          <>
+            <h1 className="font-bold">Location: {location.location}</h1>
+            <h1 className="text-textGray">EP: {location.ep}</h1>
+          </>
+        )
+       })}
+       
     </div>
   );
 };
