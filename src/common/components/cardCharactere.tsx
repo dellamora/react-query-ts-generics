206732ce25b34e33c2d3e08b0ea5a898d5b9bf0f
Dellamora <francielle@dellamora.dev>
2023-02-20 20:16:51 -0300

    fixed firsr card bug

--- src/common/components/cardCharactere.tsx
+++ src/common/components/cardCharactere.tsx
@@ -9,14 +9,14 @@ type Props = {
 
 const CardCharactere = ({ rickandmortyAPI }: Props): JSX.Element => {
   return (
-    <div className="grid-cols-charactersCards gap-8 space-y-6 lg:grid lg:w-full lg:p-20">
+    <div className="grid-cols-charactersCards gap-8 space-y-6 lg:grid lg:w-full lg:space-y-0 lg:p-20">
       {rickandmortyAPI.map(character => {
         return (
           <div
             key={`character-${character.id}`}
-            className="overflow-hidden rounded-md border border-secondary bg-secondary shadow-md"
+            className="h-full overflow-hidden rounded-md border border-secondary bg-secondary shadow-md"
           >
-            <div className="relative aspect-video w-80 overflow-hidden lg:w-full  ">
+            <div className="relative aspect-video w-80 overflow-hidden lg:w-full">
               <Image
                 alt="character"
                 src={character.image}
@@ -24,7 +24,7 @@ const CardCharactere = ({ rickandmortyAPI }: Props): JSX.Element => {
                 className="object-cover"
               />
             </div>
-            <div className=" h-full w-full p-4">
+            <div className="p-4">
               <h1 className="font-bold">{character.name}</h1>
               <h1 className="text-textGray">{character.species}</h1>
             </div>
