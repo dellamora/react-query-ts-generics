89b284bd97c53bda37a3abb93a9d97cad7de20d0
Dellamora <francielle@dellamora.dev>
2023-02-20 20:05:24 -0300

    refactor: added userickandmortyAPI and style

--- src/common/components/cardCharactere.tsx
+++ src/common/components/cardCharactere.tsx
@@ -1,15 +1,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
+import type { Character } from "../../domain/interfaces";
 import React from "react";
+import Image from "next/image";
 
-const CardCharactere: React.FC = (): JSX.Element => {
+type Props = {
+  rickandmortyAPI: Pick<Character, "image" | "name" | "species" | "id">[];
+};
+
+const CardCharactere = ({ rickandmortyAPI }: Props): JSX.Element => {
   return (
-    <div className="h-72 w-80 overflow-hidden rounded-md border-2 border-primary bg-secondary">
-      <div className="aspect-video bg-primaryRed"> imagem aqui</div>
-      <div className=" h-full w-full p-4">
-        <h1 className="font-bold">nome do pp</h1>
-        <h1 className="text-textGray">especie</h1>
-      </div>
+    <div className="grid-cols-charactersCards gap-8 space-y-6 lg:grid lg:w-full lg:p-20">
+      {rickandmortyAPI.map(character => {
+        return (
+          <div
+            key={`character-${character.id}`}
+            className="overflow-hidden rounded-md border border-secondary bg-secondary shadow-md"
+          >
+            <div className="relative aspect-video w-80 overflow-hidden lg:w-full  ">
+              <Image
+                alt="character"
+                src={character.image}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className=" h-full w-full p-4">
+              <h1 className="font-bold">{character.name}</h1>
+              <h1 className="text-textGray">{character.species}</h1>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
