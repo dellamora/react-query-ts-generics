c11519db3ccb0ad9a737810ba0121e3c06778a7e
Dellamora <francielle@dellamora.dev>
2023-02-20 19:15:53 -0300

    refactor: deleted usecharacter hook and added userickandmortyapi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { useState, useMemo } from "react";
 import Grid from "../modules/hero/grid";
-import useCharacters from "../common/hooks/useCharacters";
+import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 
 //passar o loadig pro c=grid pra caso ele estiver carregando desabilatar o caralho la do buton
 // filter pra caso de pp personagem favorito ai remove
 // IOAPI sempre button de loading more estiver visivel dar fetch
 export default function Home() {
-  const { characters, isLoading, setPage } = useCharacters();
+  const {
+    data: characters,
+    isLoading,
+    setPage,
+  } = useRickAndMortyAPI("character");
 
   return (
     <div>
