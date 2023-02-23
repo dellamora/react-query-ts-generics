4dee105033a2bcd595f25268d919e4ebc3807872
Dellamora <francielle@dellamora.dev>
2023-02-22 22:45:58 -0300

    deleted useless comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-import { useState, useMemo } from "react";
 import Grid from "../modules/hero/grid";
 import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 
-//passar o loadig pro c=grid pra caso ele estiver carregando desabilatar o caralho la do buton
-// filter pra caso de pp personagem favorito ai remove
-// IOAPI sempre button de loading more estiver visivel dar fetch
 export default function Home() {
   const {
     data: characters,
