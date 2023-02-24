f53bda777d3225983bd96f579786fe2f2f3e784b
Dellamora <francielle@dellamora.dev>
2023-02-24 20:25:47 -0300

    removed footer

--- src/layouts/baseLayout.tsx
+++ src/layouts/baseLayout.tsx
@@ -1,26 +1,17 @@
-// import { useScroll } from "framer-motion";
-import React, { useRef } from "react";
+import React from "react";
 import Nav from "../modules/nav";
-import Footer from "../modules/footer";
+
 const BaseLayout = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
-  const containerRef = useRef(null);
-  //   const { scrollYProgress } = useScroll({
-  //     container: containerRef,
-  //   });
   return (
     <div className="h-screen w-screen overflow-y-hidden">
       <div id="modal-root" />
-      <div
-        className="flex h-screen flex-col overflow-auto bg-primary "
-        ref={containerRef}
-      >
+      <div className="flex h-screen flex-col overflow-auto bg-primary ">
         <Nav />
         <div>{children}</div>
-        <Footer />
       </div>
     </div>
   );
