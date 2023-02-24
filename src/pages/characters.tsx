29c518a0b90fb5c8aa3c0f71ccf14c9acc2dd742
Dellamora <francielle@dellamora.dev>
2023-02-24 16:20:00 -0300

    deleted image

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
-import Image from "next/image";
 import SearchComponent from "../common/components/search";
 import CardCharactere from "../common/components/cardCharactere";
 import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
@@ -13,14 +12,6 @@ const characters: React.FC = (): JSX.Element => {
 
   return (
     <div className="flex flex-col items-center justify-center space-y-6 ">
-      <div className="relative h-60 w-72">
-        <Image
-          src="/portalRAM.png"
-          fill
-          alt="rick and morty title"
-          className="aspect-square"
-        />
-      </div>
       <SearchComponent />
       <CardCharactere rickandmortyAPI={characters} />
     </div>
