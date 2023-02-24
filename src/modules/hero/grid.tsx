994fc14792b3f74ddebb9bc0c5242714be4e2c1b
Dellamora <francielle@dellamora.dev>
2023-02-24 20:26:45 -0300

    refactored

--- src/modules/hero/grid.tsx
+++ src/modules/hero/grid.tsx
@@ -2,37 +2,29 @@
 
 import React, { useEffect } from "react";
 import Image from "next/image";
-import SearchComponent from "../../common/components/search";
 import { motion } from "framer-motion";
-import ButtonLoadMore from "../../common/components/buttonLoadMore";
 import type { Character } from "../../domain/interfaces";
 
 type Props = {
   rickandmortyAPI: Pick<Character, "image">[];
-  onLoadMore: () => void;
 };
 
-const Grid = ({ rickandmortyAPI, onLoadMore }: Props): JSX.Element => {
+const Grid = ({ rickandmortyAPI }: Props): JSX.Element => {
   return (
-    <>
-      <motion.div id="grid" className="h-fit p-10">
-        <div className="grid grid-cols-cards gap-10">
-          {rickandmortyAPI.map((character, i) => {
-            return (
-              <div
-                className="relative h-20 w-20 overflow-hidden rounded-lg bg-TextWhite"
-                key={`card-${i}`}
-              >
-                <Image alt="pp" src={character.image} fill />
-              </div>
-            );
-          })}
-        </div>
-      </motion.div>
-      <div className="flex justify-center">
-        <ButtonLoadMore onClick={onLoadMore} />
+    <motion.div id="grid" className="h-fit p-10">
+      <div className="grid grid-cols-cards gap-10">
+        {rickandmortyAPI.map((character, i) => {
+          return (
+            <div
+              className="relative h-20 w-20 overflow-hidden rounded-lg bg-TextWhite"
+              key={`card-${i}`}
+            >
+              <Image alt="pp" src={character.image} fill />
+            </div>
+          );
+        })}
       </div>
-    </>
+    </motion.div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
 import Grid from "../modules/hero/grid";
 import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function Home() {
   const {
     data: characters,
     isLoading,
     fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
   } = useRickAndMortyAPI("character");
+  const { ref, inView } = useInView({});
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      if (inView) fetchNextPage();
+    }, 100);
+    return () => clearTimeout(timeOut);
+  }, [inView, isFetchingNextPage]);
 
   return (
     <div>
-      <Grid
-        rickandmortyAPI={characters}
-        onLoadMore={() => {
-          if (isLoading) return;
-          fetchNextPage();
-        }}
-      />
+      <Grid rickandmortyAPI={characters} />
+      {!isLoading && !isFetchingNextPage && hasNextPage ? (
+        <div ref={ref} className="-mt-60 flex h-10 justify-center " />
+      ) : null}
     </div>
   );
 }
