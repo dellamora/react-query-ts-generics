975828aecaed28df93cb0c9cafa8f829ba9fdbb7
Dellamora <francielle@dellamora.dev>
2023-02-24 17:29:21 -0300

    feat: finish base episode page

--- src/pages/episodes.tsx
+++ src/pages/episodes.tsx
@@ -1,14 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import Card from "../common/components/Card";
 import SearchComponent from "../common/components/search";
 
 import React from "react";
-
-/* import { Container } from "./styles"; */
+import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 
 const pages: React.FC = (): JSX.Element => {
+  const {
+    data: episodes,
+    isLoading,
+    fetchNextPage,
+  } = useRickAndMortyAPI("episode");
   return (
     <div className="flex flex-col items-center justify-center space-y-6 ">
       <SearchComponent />
+      {episodes.map(episode => {
+        return (
+          <Card
+            key={`episode-${episode.id}`}
+            title={episode.name}
+            subTitle={episode.air_date}
+            append={episode.episode}
+          />
+        );
+      })}
     </div>
   );
 };
