076b1087e5894b9b71338d8edbae1978bfa79d81
Dellamora <francielle@dellamora.dev>
2023-02-24 17:54:09 -0300

    feat: finish base episodes page

--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -1,14 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
-import CardLocationandEpisodes from "../common/components/cardLocationandEpisodes";
+import Card from "../common/components/Card";
 import SearchComponent from "../common/components/search";
+import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 
 const pages: React.FC = (): JSX.Element => {
+  const {
+    data: locations,
+    fetchNextPage,
+    isLoading,
+  } = useRickAndMortyAPI("location");
   return (
     <div className="flex flex-col items-center justify-center space-y-6 ">
       <SearchComponent />
-      <CardLocationandEpisodes />
+      {locations.map(location => {
+        return (
+          <Card
+            key={`location-${location.id}`}
+            title={location.name}
+            subTitle={location.dimension}
+          />
+        );
+      })}
     </div>
   );
 };
