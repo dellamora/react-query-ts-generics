faf2d94f76989047ed53ef7eadd56d54a58617e5
Dellamora <francielle@dellamora.dev>
2023-02-26 19:40:29 -0300

    added infinity scroll at episodes and locations pages

--- src/pages/episodes.tsx
+++ src/pages/episodes.tsx
@@ -1,17 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Card from "../common/components/Card";
 import SearchComponent from "../common/components/search";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 import ContainerGrid from "../common/components/containerGrid";
+import { useInView } from "react-intersection-observer";
 
 const pages: React.FC = (): JSX.Element => {
   const [search, setSearch] = useState("");
+  const [ref, inView] = useInView({});
+
   const {
     data: episodes,
     isLoading,
     fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
   } = useRickAndMortyAPI("episode", { name: search });
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      if (inView) fetchNextPage();
+    }, 100);
+    return () => clearTimeout(timeOut);
+  }, [inView, isFetchingNextPage]);
+
   return (
     <div className="flex flex-col items-center justify-center space-y-6 ">
       <SearchComponent onChange={setSearch} />
@@ -26,6 +39,9 @@ const pages: React.FC = (): JSX.Element => {
             />
           );
         })}
+        {!isLoading && !isFetchingNextPage && hasNextPage ? (
+          <div ref={ref} className="-mt-60 flex h-10 justify-center " />
+        ) : null}
       </ContainerGrid>
     </div>
   );
