9309412497a0cd57f900cd08d962b8576b09d294
Dellamora <francielle@dellamora.dev>
2023-02-27 09:13:57 -0300

    deleted useless folder and refactored home page withou grid component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Grid from "../modules/hero/grid";
 import useRickAndMortyAPI from "../common/hooks/useRickAndMortyAPI";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Home() {
   const {
@@ -22,7 +24,22 @@ export default function Home() {
 
   return (
     <div>
-      <Grid rickandmortyAPI={characters} />
+      <motion.div id="grid" className="h-fit p-10">
+        <div className="grid grid-cols-cards gap-10">
+          {characters.map((character, i) => {
+            return (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { delay: i * 0.1 } }}
+                className="relative h-20 w-20 overflow-hidden rounded-lg bg-TextWhite"
+                key={`card-${i}`}
+              >
+                <Image alt="pp" src={character.image} fill />
+              </motion.div>
+            );
+          })}
+        </div>
+      </motion.div>
       {!isLoading && !isFetchingNextPage && hasNextPage ? (
         <div ref={ref} className="-mt-60 flex h-10 justify-center " />
       ) : null}
