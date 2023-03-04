be921d4f0ec72c394f9e09ccc88b2df5ed9d8032
Dellamora <francielle@dellamora.dev>
2023-03-04 20:24:31 -0300

    create useEpisodes hook

---
+++ src/common/components/carEpisodes.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const CardEpisodes: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, CardEpisodes!</h1>
+    </div>
+  );
+};
+
+export default CardEpisodes;
